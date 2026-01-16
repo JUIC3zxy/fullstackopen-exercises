5bc588ec143fc0e60aa9088081038f3a2253c5bc
JUIC3zxy <juleszxy@gmail.com>
2026-01-16 17:21:46 +1100

    2.10 finish

---
+++ part2/the phonebook/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+  const fileteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const nameExists = persons.some((person) => person.name === newName);
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    if (newNumber === "") {
+      alert(`Number cannot be empty`);
+      return;
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+  return (
+    <div>
+      <h2>Filter</h2>
+      <div>
+        filter: <input value={filter} onChange={handleFilterChange} />
+      </div>
+
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {fileteredPersons.map((person) => (
+        <p key={person.name}>
+          {person.name}:{person.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
